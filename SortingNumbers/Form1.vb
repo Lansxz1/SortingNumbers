f433d50c9a991d82215496f7060fd168f9e13b14
kristoffer lance bulan <hed-kbulan@smu.edu.ph>
2025-11-07 08:36:50 +0800

    Add StreaWriter to store numbers in numbers.txt

--- SortingNumbers/Form1.vb
+++ SortingNumbers/Form1.vb
@@ -1,8 +1,22 @@
 ﻿Imports System.IO
+Imports System.Linq
 Public Class Form1
+
+    Dim numbers As New List(Of Integer)
+
     Dim filepath As String = "numbers.txt"
     Private Sub btnWrite_Click(sender As Object, e As EventArgs) Handles btnWrite.Click
 
+        Try
+
+            Using writer As New StreamWriter(filepath, True)
+                writer.WriteLine(NumericUpDown1.Value)
+                writer.Close()
+            End Using
+            MessageBox.Show("Number written successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
     End Sub
 
     Private Sub btnRead_Click(sender As Object, e As EventArgs) Handles btnRead.Click
