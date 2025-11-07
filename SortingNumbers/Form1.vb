6e1d7c2f3551a0862ef702e32cbabd3692b207c7
Lansxz1 <lancebulan1928@gmail.com>
2025-11-07 09:12:48 +0800

    Added StreamReader to display numbers in ListBox

--- SortingNumbers/Form1.vb
+++ SortingNumbers/Form1.vb
@@ -1,5 +1,7 @@
 ﻿Imports System.IO
 Imports System.Linq
+Imports System.Windows.Forms.LinkLabel
+
 Public Class Form1
 
     Dim numbers As New List(Of Integer)
@@ -9,7 +11,7 @@ Public Class Form1
 
         Try
 
-            Using writer As New StreamWriter(filepath, True)
+            Using writer As New StreamWriter(filepath)
                 writer.WriteLine(NumericUpDown1.Value)
                 writer.Close()
             End Using
@@ -18,10 +20,33 @@ Public Class Form1
             MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
         End Try
     End Sub
-
     Private Sub btnRead_Click(sender As Object, e As EventArgs) Handles btnRead.Click
+        Try
+            ListBox1.Items.Clear()
+            Using reader As New StreamReader(filepath)
+                Dim line As String = reader.ReadLine()
 
+                While line IsNot Nothing
+                    Dim num As Integer
+                    If Integer.TryParse(line, num) Then
+                        numbers.Add(num)
+                    End If
+                    line = reader.ReadLine()
+                End While
+                reader.Close()
+            End Using
+
+            For Each num In numbers
+                ListBox1.Items.Add(num)
+            Next
+
+            MessageBox.Show("Numbers succesfully shown.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Catch ex As Exception
+
+            MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
     End Sub
+
 
     Private Sub btnSort_Click(sender As Object, e As EventArgs) Handles btnSort.Click
 
